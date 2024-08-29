6929a67a16a023e11969ea5403dc9f8fe49219b0
mizchi <miz404@gmail.com>
2024-08-29 15:50:13 +0900

    use pgbouncer

--- load-context.dev.ts
+++ load-context.dev.ts
@@ -6,8 +6,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 // only development mode need
 export const getLoadContext: GetLoadContext = async ({ context }) => {
   console.log("MODE: development");
+
   const pool = new pg.Pool({
-    connectionString: context.cloudflare.env.DATABASE_URL,
+    connectionString:
+      context.cloudflare.env.DATABASE_URL + "&connection_limit=1",
   });
   const adapter = new PrismaPg(pool);
   const db = new PrismaClient({ adapter });
